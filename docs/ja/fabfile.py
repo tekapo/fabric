fd74266446e67840babdd681fee88e0cbad6af88
JOTAKI Taisuke <tekapo@gmail.com>
2013-11-29 16:13:41 +0900

    translate some Japanese

--- docs/ja/fabfile.py
+++ docs/ja/fabfile.py
@@ -1,27 +1,36 @@
 from fabric.api import local, run, env
+from fabric.contrib.files import exists
 
 env.use_ssh_config = True
 
-'''
-    env.hosts = ['ssh081.lolipop.jp']
-    env.key_filename = ['~/.ssh/id_rsa']
-    env.user = 'lolipop.jp-68151c66c229bc12'
-    env.port = '2222'
-'''
+def has_auth_keys():
+    '''
+        has already the ~/.ssh/authorized_keys or not.
+        if not, mkdir .ssh and touch authorized_keys
+    '''
+    
+    if not exists("~/.ssh/authorized_keys"):
+        print 'Creating authrized_keys.'
+        run('mkdir  ~/.ssh && cd ~/.ssh && touch authorized_keys' )
+
+    if exists("~/.ssh/authorized_keys"):
+        print 'Ctreated authorized_keys.'
+    else:
+        print "Couldn't create authorized_keys." 
 
 def host_type():
     run('uname -s')
 
 def test():
-	local("echo 'test'")
+    local("echo 'test'")
 
 def commit():
-	local("git add -A && git commit -m 'translate some Japanese'")
+    local("git add -A && git commit -m 'translate some Japanese'")
 
 def push():
-	local("git push")
+    local("git push")
 
-def prepare_deploy():
-	test()
-	commit()
-	push()
+def g_cm_ph():
+    test()
+    commit()
+    push()
